2f7a93c4b56d5441566ca50ba50a9afb3b2116f1
Petri M <petri.malmstrom@gmail.com>
2022-06-20 09:54:47 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
+const QUERY = `
+{
+  stop(id: "Lahti:207190") {
+    name
+    stoptimesWithoutPatterns(numberOfDepartures: 50) {
+      stop {
+        platformCode
+      }
+      scheduledArrival
+      scheduledDeparture
+      trip {
+        route {
+          shortName
+        }
+      }
+      headsign
+    }
+  }
+}
+`
+
 function App() {
+    const [arrivals, setArrivals] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('https://api.digitransit.fi/routing/v1/routers/waltti/index/graphql', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ query: QUERY })
+        }).then(response => response.json())
+            .then(data => setArrivals(data.data.stop.stoptimesWithoutPatterns))
+    }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Arrivals, capped @ 50 </h1>
+          <ul className="List">
+              
+              {arrivals.map((arrival =>
+                  <li key={arrival.headsign}>{Math.floor(arrival.scheduledArrival / 3600)} {Math.floor(arrival.scheduledArrival % 3600 / 60 )}</li>
+              ))}
+          </ul>  
     </div>
   );
 }
